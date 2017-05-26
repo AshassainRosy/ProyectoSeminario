9d4157cacefb128a977062247efeb4a5183080af
milenka1994 <mireyamilenka@gmail.com>
2017-05-26 16:48:04 -0400

    cambios mile

--- client/administrador/contentAdminUser.js
+++ client/administrador/contentAdminUser.js
@@ -7,9 +7,5 @@ Template.contentAdminUsers.onRendered(function(e){
 	$("body").removeClass("cyan");
 });
 Template.contentAdminUsers.events({
-	"click .btn-floating":function(e){
-		var row=$(this).parents('tr');
-		var _id=row.prevObject[0]._id;
-		
-	}
+	
 });
